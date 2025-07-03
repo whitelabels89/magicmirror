88fbd45f7cdcdd72d59fdc5e4aaf7c660eafbaed
whitelabels89 <whitelabels.89@gmail.com>
2025-07-03 11:45:02 +0700

    panel profile

---
+++ magicmirror-node/public/elearn/panel-profile.js
@@ -0,0 +1,19 @@
+
+
+
+function initPanelProfile() {
+  const panel = document.querySelector('.panel-ungu');
+  const btn = document.getElementById('toggle-panel-btn');
+  function updateBtnDirection() {
+    if (panel.classList.contains('hidden')) {
+      btn.innerHTML = '⮞';
+    } else {
+      btn.innerHTML = '⮜';
+    }
+  }
+  btn.addEventListener('click', () => {
+    panel.classList.toggle('hidden');
+    updateBtnDirection();
+  });
+  updateBtnDirection();
+}
