69cc0ca449d03a654e2e561d512cda237048e866
whitelabels89 <whitelabels.89@gmail.com>
2025-08-14 19:57:44 +0700

    A2

--- magicmirror-node/public/elearn/manifest-lessons.js
+++ magicmirror-node/public/elearn/manifest-lessons.js
@@ -2,6 +2,7 @@
 // Boleh dikosongkan; kalau ada entri, harus override deteksi otomatis.
 window.LESSON_MANIFEST = {
   "/elearn/calistung/level/A1.html": { course_id: "numbers-basic", lesson_id: "L1" },
+  "/elearn/calistung/level/A2.html": { course_id: "numbers-basic", lesson_id: "L2" },
   // Contoh lainnya:
   // "/elearn/worlds/calistung/number/A4a.html": { course_id: "numbers-basic", lesson_id: "L4" },
   // "/elearn/worlds/calistung/number/B2.html": { course_id: "numbers-basic", lesson_id: "L7" },
